6335d31c62287b4db7db970fe1fd216d5e10cb5e
Dhiman-22 <dimanvivek1697@gmail.com>
2023-07-21 19:13:27 +0530

    day-2

---
+++ olxclone/src/Route/productDetails.jsx
@@ -0,0 +1,110 @@
+import {
+    Box,
+    Container,
+    Stack,
+    Text,
+    Image,
+    Flex,
+    VStack,
+    Button,
+    Heading,
+    SimpleGrid,
+    StackDivider,
+    useColorModeValue,
+    List,
+    ListItem,
+  } from '@chakra-ui/react';
+  import { MdLocalShipping } from 'react-icons/md';
+// import { Navigate } from 'react-router';
+  
+  export default function ProductDetails({image,category,price,description}) {
+  
+    return (
+      <Container maxW={'7xl'}>
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 18, md: 24 }}>
+          <Flex border="1px solid gray">
+            <Image
+              rounded={'md'}
+              alt={category}
+              src={
+                image
+              }
+              align={'center'}
+              w={'100%'}
+              h={{ base: '100%', sm: '400px', lg: '500px' }}
+            />
+          </Flex>
+          <Stack spacing={{ base: 6, md: 10 }}>
+            <Box as={'header'}>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+               {category}
+              </Heading>
+              <Text
+                fontWeight={300}
+                fontSize={'2xl'}>
+                ${price}
+              </Text>
+            </Box>
+  
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={'column'}
+              divider={
+                <StackDivider
+                />
+              }>
+              <VStack spacing={{ base: 4, sm: 6 }}>
+                
+                <Text fontSize={'lg'}>
+                    {description}
+                </Text>
+              </VStack>
+              <Box>
+                
+  
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                  <List spacing={2}>
+                    <ListItem>Chronograph</ListItem>
+                    <ListItem>Master Chronometer Certified</ListItem>{' '}
+                    <ListItem>Tachymeter</ListItem>
+                  </List>
+                  <List spacing={2}>
+                    <ListItem>Anti‑magnetic</ListItem>
+                    <ListItem>Chronometer</ListItem>
+                    <ListItem>Small seconds</ListItem>
+                  </List>
+                </SimpleGrid>
+              </Box>
+              <Box>
+              </Box>
+            </Stack>
+            <Stack direction="row" alignItems="center" justifyContent={'center'}>
+              <MdLocalShipping />
+              <Text>2-3 business days delivery</Text>
+            </Stack>
+          </Stack>
+          <Button
+            rounded={'none'}
+            w={'full'}
+            mt={8}
+            size={'lg'}
+            py={'7'}
+            bg={useColorModeValue('gray.900', 'gray.50')}
+            color={useColorModeValue('white', 'gray.900')}
+            textTransform={'uppercase'}
+            _hover={{
+              transform: 'translateY(2px)',
+              boxShadow: 'lg',
+            }}>
+            Add to cart
+          </Button>
+        </SimpleGrid>
+      </Container>
+    );
+  }
